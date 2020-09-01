e7fd7249187baa4d7b8c4ea9004165aa998b8e51
Kristin Eng <kristineng@eduroam-193-157-240-140.wlan.uio.no>
2020-09-01 13:30:31 +0200

    Lagde repr, add og eqmed tester

--- vector.py
+++ vector.py
@@ -10,8 +10,15 @@ class Vector3D:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x},{self.y},{self.z})"
 
+    def __add__(self,other):
+        return Vector3D(self.x + other.x,self.y + other.y,self.z + other.z)
 
+    def __eq__(self,other):
+        """Return True if self == other, otherwise return False"""
+        return self.x == other.x and self.y == other.y and self.z == other.z
 
 if __name__ == "__main__":
     v = Vector3D(1,4,2)
-    print([v])
+    u = Vector3D(1,1,1)
+    w = u + v
+    w == Vector3D(2,5,)
