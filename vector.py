86ad0a6d7c81ff2068613e5d5147426207ab4f45
Kristin Eng <kristineng@eduroam-193-157-240-140.wlan.uio.no>
2020-09-01 12:50:23 +0200

    Laget klasse og to tester

---
+++ vector.py
@@ -0,0 +1,17 @@
+class Vector3D:
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        return f"({self.x},{self.y},{self.z})"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.x},{self.y},{self.z})"
+
+
+
+if __name__ == "__main__":
+    v = Vector3D(1,4,2)
+    print([v])
